79013fd863f4238e79594dc7f673d3fe90056bae
Dylan K. Taylor <odigiman@gmail.com>
2020-06-14 10:54:56 +0100

    Initial commit

---
+++ src/Tile.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ *
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
+ *
+ *
+*/
+
+declare(strict_types=1);
+
+/**
+ * All the Tile classes and related classes
+ */
+
+namespace pocketmine\tile;
+
+abstract class Tile{
+
+	/**
+	 * Reads additional data from the CompoundTag on tile creation.
+	 */
+	abstract protected function readSaveData() : void;
+
+	/**
+	 * Writes additional save data to a CompoundTag, not including generic things like ID and coordinates.
+	 */
+	abstract protected function writeSaveData() : void;
+}
